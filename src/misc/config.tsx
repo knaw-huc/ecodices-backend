06c938fd11c0f149057e9e5e25cceea08f69a035
Rob Zeeman <rob.zeeman@di.huc.knaw.nl>
2022-02-08 11:08:08 +0100

    Update config.tsx

--- src/misc/config.tsx
+++ src/misc/config.tsx
@@ -3,3 +3,4 @@ export const SERVICE_SERVER = "https://ecoservice.sd.di.huc.knaw.nl/";
 export const HOME = "http://localhost:3000/";
 //export const HOME = "https://ecodices.sd.di.huc.knaw.nl/";
 export const EDITOR = "http://www.huc.localhost/huc-editor/editor.php?profile=ecodices&record=";
+//export const EDITOR = "https://ecodices.sd.di.huc.knaw.nl/editor/editor.php?profile=ecodices&record=";
